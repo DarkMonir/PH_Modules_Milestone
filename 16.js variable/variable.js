6a8e77eadb52759d57ea8261919b5831850805c8
DarkMonir <monirmunna2000@gmail.com>
2024-01-26 00:16:31 +0600

    javascript variable

---
+++ 16.js variable/variable.js
@@ -0,0 +1,16 @@
+var weight = 38 ;
+var price = 38;
+var money = 2000;
+
+money = 500;
+
+console.log(price)
+
+// String
+
+var name = "Monir";
+var address = 'iktiar e asi boktiar e jaibo';
+
+var isRich = true;
+console.log(isRich)
+console.log(typeof isRich)
