f1e1525109a74e144a155f37d98f612279b3780f
DarkMonir <monirmunna2000@gmail.com>
2024-01-30 23:51:46 +0600

    javascript array 1

---
+++ JS/Array/array.js
@@ -0,0 +1,13 @@
+
+const language = ['HTML', 'CSS', 'JavaScript', 'PHP', true, 50]
+
+language[4] = 'Tailwind CSS';
+language[5] = 'react.js';
+language[6] = 'vue.js';
+language[7] = 'laravel';
+language[10] = 'Bootstrap';
+
+console.log(language[language.length - 1]);
+
+console.log(language)
+console.log(language.toString())
