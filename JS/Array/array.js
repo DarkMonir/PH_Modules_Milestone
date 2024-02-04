b1aabbb228a90a299154a8b9b7591805a81ee859
DarkMonir <monirmunna2000@gmail.com>
2024-02-05 02:55:58 +0600

    javascript

--- JS/Array/array.js
+++ JS/Array/array.js
@@ -79,35 +79,86 @@
 // console.log(Array.isArray(age))
 
 /// ////    Declare an array
-const fruits = ['apple', 'banana', 'mango', 'olive', 'orange']
+// const fruits = ['apple', 'banana', 'mango', 'olive', 'orange']
 
-console.log(fruits[3])
+// console.log(fruits[3])
 
-fruits[2] = 'jambura';
-console.log(fruits)
+// fruits[2] = 'jambura';
+// console.log(fruits)
 
 /////////////          add or remove elements
 
-const destination = ['cox-bazar', 'rangamati', 'syllet']
+// const destination = ['cox-bazar', 'rangamati', 'syllet']
 
-destination.push('hwaii')
-destination.push('ami', 'jami')
-destination.pop()
-console.log(destination)
+// destination.push('hwaii')
+// destination.push('ami', 'jami')
+// destination.pop()
+// console.log(destination)
 
 
 ///   includes
 
-const books = ['atomic habits', 'javascript', 'physics', 'chemistry']
+// const books = ['atomic habits', 'javascript', 'physics', 'chemistry']
 
-console.log(books.includes('javascript'))
+// console.log(books.includes('javascript'))
 
 /////////   concat
 
-const money =['22', '44', '77', '99']
-const friends = ['balam', 'kalam', 'chailam', 'gelam', 'pailam']
+// const money =['22', '44', '77', '99']
+// const friends = ['balam', 'kalam', 'chailam', 'gelam', 'pailam']
 
-console.log(money)
-console.log(friends)
+// console.log(money)
+// console.log(friends)
 
-console.log(money.concat(friends))
+// console.log(money.concat(friends))
+
+
+
+// const friends = ['Elon', 'Bill', 'Mark', 'Waren'];
+
+// for(const friend of friends){
+//     console.log(friend);
+// }
+
+// for(let i = 0; i < friends.length; i++){
+//     console.log(i);
+//     console.log(friends[i])
+// }
+
+//              [9, 8, 7, 6, 5, 4, 3, 2, 1]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(numbers);
+
+// const reversed = numbers.reverse();
+
+// console.log(reversed);
+
+
+// const rev_numbers = [];
+
+// for(const num of numbers){
+
+//     rev_numbers.unshift(num)
+// }
+// console.log(rev_numbers)
+
+
+const reverse_numbers = [];
+for(let i = 0; i < numbers.length; i++){
+    const num = numbers[i];
+    reverse_numbers.unshift(num);
+}
+console.log(reverse_numbers)
+
+
+
+// reverse side
+const rev_reverse_numbers =[]
+for(let i = numbers.length -1 ; i <= 0; i++){
+    const num = numbers[i];
+    // reverse_numbers.shift(num);
+    console.log(num);
+    rev_reverse_numbers.push(num);
+}
+console.log(rev_reverse_numbers)
